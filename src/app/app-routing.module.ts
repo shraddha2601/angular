c2ac708d7d67656134066f67eff1e3946beeba72
shraddha gediya <rupesh.halpati@aspiresoftserv.com>
2022-06-02 18:39:44 +0530

    changes in signup component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { SignUpUserComponent } from './sign-up-user/sign-up-user.component';
 
 const routes: Routes = [
   {path : ' ', redirectTo: 'login', pathMatch : 'full'}, 
   {path : 'login', component: LoginComponent},
   {path : 'signup', component: SignupComponent},
   {path : 'dashboard', component: EmployeeDashboardComponent},
+  {path : 'signuplist', component: SignUpUserComponent},
 ]
 
 @NgModule({
